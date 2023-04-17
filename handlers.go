310d4d9ddbb3c408710d3d6bc73d39de4bc25c27
Krzysi3k <global.takeover@o2.pl>
2023-04-17 11:38:20 +0200

    refactor

--- handlers.go
+++ handlers.go
@@ -23,11 +23,11 @@ func GetRedisData(ctx context.Context, rdb *redis.Client) echo.HandlerFunc {
 	return func(e echo.Context) error {
 		keyName := e.QueryParam("data")
 		if keyName == "" {
-			return e.JSON(400, map[string]interface{}{"payload": "missing query string"})
+			return e.JSON(400, map[string]string{"payload": "missing query string"})
 		}
 		val, err := rdb.Get(ctx, keyName).Result()
 		if err != nil {
-			return e.JSON(404, map[string]interface{}{"payload": "key not found"})
+			return e.JSON(404, map[string]string{"payload": "key not found"})
 		}
 		if strings.Contains(keyName, "docker:metrics:") || keyName == "termometr-payload" {
 			return e.String(200, val)
@@ -35,7 +35,7 @@ func GetRedisData(ctx context.Context, rdb *redis.Client) echo.HandlerFunc {
 		if json.Valid([]byte(val)) {
 			return e.JSONBlob(200, []byte(val))
 		} else {
-			return e.JSON(200, map[string]interface{}{"payload": val})
+			return e.JSON(200, map[string]string{"payload": val})
 		}
 	}
 }
@@ -83,7 +83,7 @@ func GetDockerInfo(ctx context.Context, dockerClient *client.Client) echo.Handle
 				line := fmt.Sprintf("%v - %v", conName, container.Status)
 				containers = append(containers, line)
 			}
-			return c.JSON(200, map[string]interface{}{"containers": containers})
+			return c.JSON(200, map[string][]string{"containers": containers})
 		case "images":
 			imagesList, err := dockerClient.ImageList(ctx, types.ImageListOptions{All: true})
 			logError(err)
@@ -95,9 +95,9 @@ func GetDockerInfo(ctx context.Context, dockerClient *client.Client) echo.Handle
 				line := fmt.Sprintf("%v, %vMB", img.RepoTags[0], (img.Size / 1024 / 1024))
 				images = append(images, line)
 			}
-			return c.JSON(200, map[string]interface{}{"images": images})
+			return c.JSON(200, map[string][]string{"images": images})
 		default:
-			return c.JSON(400, map[string]interface{}{"payload": "wrong or missing query param"})
+			return c.JSON(400, map[string]string{"payload": "wrong or missing query param"})
 		}
 	}
 }
@@ -108,7 +108,7 @@ func RemoveContainer(ctx context.Context, dockerClient *client.Client) echo.Hand
 		nameParam := c.QueryParam("name")
 		stopParam := c.QueryParam("stop")
 		if nameParam == "" {
-			return c.JSON(400, map[string]interface{}{"payload": "missing query param"})
+			return c.JSON(400, map[string]string{"payload": "missing query param"})
 		}
 		containerList, err := dockerClient.ContainerList(ctx, types.ContainerListOptions{All: true})
 		if err != nil {
@@ -119,25 +119,25 @@ func RemoveContainer(ctx context.Context, dockerClient *client.Client) echo.Hand
 				if cname == "/"+nameParam {
 					if stopParam == "true" {
 						if err := dockerClient.ContainerRemove(ctx, cont.ID, types.ContainerRemoveOptions{Force: true}); err != nil {
-							return c.JSON(200, map[string]interface{}{"info": fmt.Sprint(err)})
+							return c.JSON(200, map[string]string{"info": fmt.Sprint(err)})
 						}
 					} else {
 						if err := dockerClient.ContainerRemove(ctx, cont.ID, types.ContainerRemoveOptions{}); err != nil {
-							return c.JSON(200, map[string]interface{}{"info": fmt.Sprint(err)})
+							return c.JSON(200, map[string]string{"info": fmt.Sprint(err)})
 						}
 					}
-					return c.JSON(200, map[string]interface{}{"removed": nameParam})
+					return c.JSON(200, map[string]string{"removed": nameParam})
 				}
 			}
 		}
-		return c.JSON(404, map[string]interface{}{"not found": nameParam})
+		return c.JSON(404, map[string]string{"not found": nameParam})
 	}
 }
 
 func UpContainerStack(ctx context.Context, dockerClient *client.Client) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		exec.Command("sh", composePath+"/up.sh").Run()
-		return c.JSON(200, map[string]interface{}{"command": "docker-compose up"})
+		return c.JSON(200, map[string]string{"command": "docker-compose up"})
 	}
 }
 
@@ -145,7 +145,7 @@ func DeleteContainerMetrics(ctx context.Context, rdb *redis.Client) echo.Handler
 	return func(c echo.Context) error {
 		metricKeys := rdb.Keys(ctx, "docker:metrics*").Val()
 		rdb.Del(ctx, metricKeys...)
-		return c.JSON(200, map[string]interface{}{"metrics-removed": metricKeys})
+		return c.JSON(200, map[string][]string{"metrics-removed": metricKeys})
 	}
 }
 
