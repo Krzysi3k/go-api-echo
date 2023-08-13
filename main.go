3479f0172750dfe92d8c77ec741667bcd6acac1a
Krzysi3k <global.takeover@o2.pl>
2023-08-14 01:08:06 +0200

    api grouping

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 
-	//	"os"
 	"github.com/docker/docker/client"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
@@ -33,26 +32,20 @@ func main() {
 		Format: `{"date":"${time_rfc3339}","ip":"${remote_ip}","method":"${method}","status":"${status}","response_time":"${latency_human}","uri":"${uri}","agent":"${user_agent}"}` + "\n",
 	}))
 
-	err = godotenv.Load("/home/krzysiek/go-api-echo/.env")
+	err = godotenv.Load("/home/krzysiek/go-apiV1-echo/.env")
 	if err != nil {
 		log.Fatal("cannot load .env file")
 	}
 
-	//	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-	//		if username == os.Getenv("API_USERNAME") && password == os.Getenv("API_PASSWORD") {
-	//			return true, nil
-	//		}
-	//		return false, nil
-	//	}))
-
-	e.GET("/api/v1/get-redis-data", GetRedisData(ctx, rdb))
-	e.GET("/api/v1/redis-info", GetRedisInfo(ctx, rdb))
-	e.GET("/api/v1/docker-info", GetDockerInfo(ctx, dockerClient))
-	e.GET("/api/v1/containers-up", UpContainerStack(ctx, dockerClient))
-	e.DELETE("/api/v1/container", RemoveContainer(ctx, dockerClient))
-	e.DELETE("/api/v1/container-metrics", DeleteContainerMetrics(ctx, rdb))
-	e.GET("/api/v1/random", randomHandler(ctx, rdb))
-	e.GET("/api/v1/metrics", echo.WrapHandler(promhttp.Handler()))
+	apiV1 := e.Group("/apiV1/v1")
+	apiV1.GET("/get-redis-data", GetRedisData(ctx, rdb))
+	apiV1.GET("/redis-info", GetRedisInfo(ctx, rdb))
+	apiV1.GET("/docker-info", GetDockerInfo(ctx, dockerClient))
+	apiV1.GET("/containers-up", UpContainerStack(ctx, dockerClient))
+	apiV1.DELETE("/container", RemoveContainer(ctx, dockerClient))
+	apiV1.DELETE("/container-metrics", DeleteContainerMetrics(ctx, rdb))
+	apiV1.GET("/random", randomHandler(ctx, rdb))
+	apiV1.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
 	e.Logger.Fatal(e.Start(":5001"))
 }
